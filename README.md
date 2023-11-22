228b2df62e6a744f22aa3fd5e11956ae1214ef3f
fangzheng <fangzheng@nrp-pet.cn>
2023-11-22 09:24:35 +0800

    first add

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# xianyu_spider
-闲鱼APP爬虫
+<p align="center">
+    <a target="_blank" href="https://www.python.org/downloads/release/python-3810/"><img src="https://img.shields.io/badge/Python-3.x-blue.svg" /></a>
+    <a target="_blank" href='https://github.com/fangzheng0518/xianyu_spider'><img src="https://img.shields.io/github/stars/fangzheng0518/xianyu_spider.svg?style=social"/></a>
+    <a target="_blank" href="LICENSE"><img src="https://img.shields.io/:license-GPLv3-blue.svg"></a>
+</p>
+
+# 闲鱼APP数据爬取
+
+[简介](#简介) | [特性](#特性) | [技术架构](#技术架构) | [快速开始](#快速开始) | [使用说明](#使用说明) | [免责声明](#免责声明) | [常见问题](#常见问题)
+
+# 简介
+
+一个基于uiautomator2的闲鱼APP爬虫项目，严禁将所得数据商用！ 本项目只供学习使用，作者不承担任何由此带来的法律风险和损失！
+
+# 特性
+
+- [x] 支持任意关键词采集
+- [x] 支持数据导出Excel
+- [x] 支持自定义上滑次数
+
+# 技术架构
+
+- Python3.6+
+- uiautomator2
+
+# 快速开始
+
+## 1、环境准备
+
+1. clone本项目代码
+2. 在项目根目录创建Python虚拟环境venv
+    1. `cd xianyu_spider`
+    2. `pip install virtualenv`
+    3. `virtualenv venv`
+3. 安装依赖库`pip install -r requirements.txt`
+4. 本次使用的是uiautomator2和USB真机调试，请打开Android手机USB调试模式并连接到电脑
+5. 首次通过uiautomator2连接手机时，会自动在手机上安装一个小黄车APP
+6. 打开cmd输入`adb devices`获取设备ID
+
+## 2、运行项目
+
+1. 将设备ID输入xianyu.py的`d = u2.connect("设备ID")`中
+2. 运行`python xianyu.py`即可进行采集，如需更改采集数据，可更改main函数keyword即可`main(keyword='餐饮券')`
+
+# 使用说明
+
+1. 本次使用的是uiautomator2和USB真机调试，请打开手机USB调试模式并连接到电脑
+2. 本次只采集了每个商品的【标题、价格、图片】
+3. 如果想采集其他信息，可使用weditor进行调试，自行更改相应代码，命令行输入`weditor`自动打开调试辅助工具
+   ![整体截图](example/weditor.png "统计信息")
+4. 程序运行完成后，会在当前目录下生成`YYYY-MM-dd结果.xlsx`
+
+## 运行截图
+
+- 运行截图
+  ![整体截图](example/run1.png "运行截图")
+- 运行结果
+  ![整体截图](example/result.png "运行结果")
+
+# 常见问题
+
+## 错误：atxagent 下次必须通过adb启动
+
+电脑USB连接安卓设备；
+
+1. 查看设备是否成功连接：`adb devices`
+   出现的真实设备名称在device前即连接成功；
+2. 进入手机shell环境：`adb shell`
+3. 赋予设备权限：`chmod 775 /data/local/tmp/atx-agent`
+4. 后台启动atx-agent： `/data/local/tmp/atx-agent server -d`
+
+## 错误：device显示unauthorized
+
+如果输入`adb devices`显示unauthorized表示usb调试模式授权有问题，按如下方式调整
+
+```shell
+1、手机里点击【撤销USB调试授权】
+2、电脑上重启adb
+ adb kill-server
+ adb start-server
+3、然后重新连接
+```
+
+# 免责声明
+
+请勿将`xianyu_spider`应用到任何可能会违反法律规定和道德约束的工作中，请友善使用`xianyu_spider`，遵守蜘蛛协议，不要将`xianyu_spider`用于任何非法用途。如您选择使用`xianyu_spider`
+即代表您遵守此协议，作者不承担任何由于您违反此协议带来任何的法律风险和损失，一切后果由您承担。
+
---
+++ xianyu.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import pathlib
+import random
+import re
+import shutil
+import string
+import sys
+import textwrap
+import time
+from datetime import datetime, date, timedelta
+
+import colorlog
+import uiautomator2 as u2
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image
+from openpyxl.drawing.spreadsheet_drawing import AnchorMarker, TwoCellAnchor
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+formatter = colorlog.ColoredFormatter(
+    fmt='%(log_color)s %(asctime)s %(levelname)s:%(message)s%(reset)s',
+    datefmt="%Y-%m-%d %H:%M:%S",
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+d = u2.connect("SNU0220A15007866")
+dinfo = d.info
+d_displayHeight = dinfo['displayHeight']
+d_displayWidth = dinfo['displayWidth']
+
+package_name = "com.taobao.idlefish"
+activity_name = ".maincontainer.activity.MainActivity"
+# 向上滑动最大次数
+MAX_PAGE = 5
+
+
+class TimeUtil:
+
+    @staticmethod
+    def random_sleep(random_start=2, random_end=5):
+        wait_time = random.randint(random_start, random_end)
+        time.sleep(wait_time)
+
+    @staticmethod
+    def sleep(secs):
+        time.sleep(secs)
+
+    @staticmethod
+    def curr_date():
+        return datetime.now().strftime("%Y-%m-%d")
+
+    @staticmethod
+    def tomorrow_date():
+        today = date.today()
+        tomorrow = today + timedelta(days=1)
+        return tomorrow.strftime('%Y-%m-%d')
+
+
+def get_desktop_path():
+    if sys.platform == 'win32':
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    elif sys.platform == 'darwin':
+        desktop_path = str(pathlib.Path.home() / "Desktop")
+    else:
+        desktop_path = None
+    return desktop_path
+
+
+def excel_cell_to_index(cell_str):
+    """
+    Excel单元格转换为行列索引，例如A1转换为1,1  C4转换为3,4
+    :param cell_str:
+    :return:
+    """
+    letter = cell_str[0]
+    number = int(cell_str[1:])
+    column = ord(letter.upper()) - ord('A') + 1
+    row = number
+    return row, column
+
+
+def write_img_by_cell(work_book, sheet_name, cell_str, img_path, target_file):
+    """
+    向Excel某单元格写入图片
+    :param work_book: work_book
+    :param sheet_name: sheet页名称
+    :param cell_str: 单元格名称，例A1，B2，C3
+    :param img_path: 图片全路径
+    :return:
+    """
+    sheet = work_book[sheet_name]
+    row, column = excel_cell_to_index(cell_str)
+    row = row - 1
+    column = column - 1
+    img = Image(img_path)
+    _from = AnchorMarker(column, 40000, row, 40000)
+    to = AnchorMarker(column + 1, -40000, row + 1, -40000)
+    img.anchor = TwoCellAnchor('twoCell', _from, to)
+    sheet.add_image(img)
+    work_book.save(target_file)
+
+
+def to_excel(data_list):
+    dt = TimeUtil.curr_date()
+    write_path = os.getcwd()
+    if not os.path.exists(write_path):
+        os.makedirs(write_path)
+    output_file = os.path.join(write_path, f"{dt}结果.xlsx")
+    wb = Workbook()
+    sheet = wb.active
+    sheet_name = 'Sheet1'
+    sheet.title = sheet_name
+    sheet['A1'] = '标题'
+    sheet['B1'] = '价格'
+    sheet['C1'] = '图片'
+    start_row = 2
+    for index, data in enumerate(data_list):
+        sheet["A" + str(index + start_row)] = data['title']
+        sheet["B" + str(index + start_row)] = data['amount']
+        write_img_by_cell(work_book=wb, sheet_name=sheet_name, cell_str='C' + str(index + start_row),
+                          img_path=data['img'],
+                          target_file=output_file)
+
+    wb.save(filename=output_file)
+
+
+def swipe(startx, starty, endx, endy):
+    d.swipe(startx, starty, endx, endy)
+
+
+def swipe_up():
+    fx = random.randint(200, 600)
+    fy = random.randint(d_displayHeight - 500, d_displayHeight - 400)
+    tx = random.randint(500, 700)
+    ty = random.randint(d_displayHeight - 1200, d_displayHeight - 1000)
+    swipe(startx=fx, starty=fy, endx=tx, endy=ty)
+
+
+def del_temp_file():
+    if os.path.exists('images'):
+        shutil.rmtree('images')
+
+
+def open_page_by_keyword(keyword):
+    TimeUtil.random_sleep()
+    d(resourceId="com.taobao.idlefish:id/title").click()
+    d.send_keys(keyword, clear=True)
+    d.press('enter')
+
+
+def generate_random_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for i in range(length))
+
+
+def get_amount(s):
+    match = re.search(r'¥(\d+)', s)
+    if match:
+        amount = match.group(1)
+        return amount
+
+
+def save_image(pil_image):
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    img_path = os.path.join('images', generate_random_string(10) + str(int(time.time())) + ".png")
+    pil_image.save(img_path)
+    return img_path
+
+
+def get_list_data():
+    result = []
+    TimeUtil.random_sleep()
+    view_list = d.xpath(
+        '//android.widget.ScrollView/android.view.View[2]/android.view.View[1]/android.widget.ImageView[1]/android.view.View[1]/android.view.View[1]/*').all()
+    if len(view_list) > 0:
+        for el in view_list:
+            item_info = el.info
+            el_description = str(item_info['contentDescription']).replace('\n', '')
+            el_text = str(item_info['text']).replace('\n', '')
+            if el_description != "" and el_description != "筛选":
+                img_path = save_image(el.screenshot())
+                result.append({
+                    'title': el_description,
+                    'amount': get_amount(el_description),
+                    'img': img_path
+                })
+
+    return result
+
+
+def usage():
+    message = """
+    ######################################################################################################################
+                                                   免责声明                                                               
+    此工具仅限于学习研究，用户需自己承担因使用此工具而导致的所有法律和相关责任！作者不承担任何法律责任！                 
+    ######################################################################################################################
+    """
+    logger.error(textwrap.dedent(message))
+    while True:
+        user_input = input("如果您同意本协议, 请输入Y继续: (y/n) ")
+        if user_input.lower() == "y":
+            break
+        elif user_input.lower() == "n":
+            sys.exit(0)
+
+
+def main_exit():
+    d.set_fastinput_ime(False)
+    d.app_stop(package_name)
+
+
+def main(keyword):
+    usage()
+    try:
+        del_temp_file()
+        logger.info(d.info)
+        d.app_stop(package_name)
+        d.app_start(package_name, activity_name, wait=True)
+        outputs = []
+
+        logger.info(f"正在获取【{keyword}】关键字信息...")
+        open_page_by_keyword(keyword)
+        for i in range(MAX_PAGE):
+            list_data = get_list_data()
+            if list_data:
+                for c in list_data:
+                    if c is not None and c != "":
+                        outputs.append(c)
+                swipe_up()
+
+        to_excel(outputs)
+    except Exception as e:
+        print(e)
+        logger.error("程序运行异常:" + str(e.args[0]))
+    finally:
+        main_exit()
+
+
+if __name__ == '__main__':
+    main(keyword='餐饮券')
